9201516ec6963e46e2d0cfaa92d4d753832fb6bc
Thijs Lausegger <326550@student.mboutrecht.nl>
2019-11-05 10:57:04 +0100

    no message

---
+++ Assets/SpinningObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningObject : MonoBehaviour
+{
+    private float Speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Speed, 0, 0);
+    }
+}
